6423323c35db34ab8916f2f551c53bac02e2a696
chinmj <chinmayvjoshi0712@gmail.com>
2020-09-08 12:50:18 +0530

    Online book store portal

---
+++ OnlineBookStore/Controllers/BookController.cs
@@ -0,0 +1,29 @@
+﻿using OnlineBookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineBookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public ActionResult Index()
+        {
+            BookDbContext dbContext = new BookDbContext();
+            List<Book> bookList = dbContext.Books.ToList();
+            return View(bookList);
+        }
+
+        // GET: Book/Details
+        
+        public ActionResult Details(int bookId)
+        {
+            BookDbContext dbContext = new BookDbContext();
+            Book book = dbContext.Books.Single(x => x.BookId == bookId);
+            return View(book);
+        }
+    }
+}
